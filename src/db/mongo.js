b109c667f9eb292decc4bad2bdab3e79f3e9d7b8
Chacaponquin <hectorangel2001@gmail.com>
2022-04-14 17:04:26 -0400

    Connection Error Fixed

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 mongoose
     .connect(
-        "mongodb+srv://Chacaponquin:hectico2511@cluster0.zdvnn.mongodb.net/Angular-Image-Api?retryWrites=true&w=majority"
+        `mongodb+srv://${process.env.MONOG_PASS}@cluster0.zdvnn.mongodb.net/Angular-Image-Api?retryWrites=true&w=majority`
     )
     .then(() => console.log("DB connected"))
     .catch((err) => console.log(err));
